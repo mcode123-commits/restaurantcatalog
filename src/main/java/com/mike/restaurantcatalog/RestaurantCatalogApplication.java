be974d06157236231a257cd736c541e13aaf6fb8
mankusmichal <mankusmichal@gmail.com>
2025-11-07 12:12:31 +0100

    spring init

---
+++ src/main/java/com/mike/restaurantcatalog/RestaurantCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mike.restaurantcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/restaurantcatalog/RestaurantCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.restaurantcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
