c5fea7f2f2776c4fbc3d172aba84337de2341536
mankusmichal <mankusmichal@gmail.com>
2025-11-07 12:23:32 +0100

    controller created

---
+++ src/main/java/com/mike/restaurantcatalog/controller/RestaurantController.java
@@ -0,0 +1,16 @@
+package com.mike.restaurantcatalog.controller;
+
+
+import com.mike.restaurantcatalog.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+}
